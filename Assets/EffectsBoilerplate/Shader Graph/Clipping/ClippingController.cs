f93ea61470ad4c0ec0db0a2024a148d818fd9b2f
Pau <paufenollosa@gmail.com>
2022-07-17 17:33:49 +0200

    Some vfx

---
+++ Assets/EffectsBoilerplate/Shader Graph/Clipping/ClippingController.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+
+public class ClippingController : MonoBehaviour
+{
+    public float m_MinClippingLevel = 0;
+    public float m_MaxClippingLevel = 2.0f;
+
+    [Range(1.0f, 5.0f)]
+    public float m_ClippingVelocity = 2.0f;
+
+
+    private Coroutine m_CurrentCoroutine = null;
+
+    void Update()
+    {
+        if (m_CurrentCoroutine == null && Input.GetKeyDown(KeyCode.H))
+        {
+            m_CurrentCoroutine = StartCoroutine(HideModel(m_MinClippingLevel, m_MaxClippingLevel));
+        }
+        else if (m_CurrentCoroutine == null && Input.GetKeyDown(KeyCode.S)) 
+        {
+            m_CurrentCoroutine = StartCoroutine(ShowModel(m_MinClippingLevel, m_MaxClippingLevel));
+        }
+    }
+
+
+
+    private IEnumerator HideModel(float minClippingLevel, float maxClippingLevel)
+    {
+        float clippingLevel = maxClippingLevel;
+
+        while (clippingLevel > minClippingLevel)
+        {
+
+            clippingLevel -= Mathf.Abs(maxClippingLevel - minClippingLevel) / m_ClippingVelocity * Time.deltaTime;
+
+            ////For the parent:
+
+            SkinnedMeshRenderer parentSkinnedMeshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+
+            if (parentSkinnedMeshRenderer != null)
+            {
+                foreach (Material material in parentSkinnedMeshRenderer.materials)
+                {
+                    material.SetFloat("_ClippingLevel", clippingLevel);
+                }
+            }
+
+            MeshRenderer parentMeshRenderer = this.GetComponent<MeshRenderer>();
+
+            if (parentMeshRenderer != null)
+            {
+                foreach (Material material in parentMeshRenderer.materials)
+                {
+                    material.SetFloat("_ClippingLevel", clippingLevel);
+                }
+            }
+
+
+            //For childs:
+
+            //Skinned mesh renderer:
+            SkinnedMeshRenderer[] skinnedMeshRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            if (skinnedMeshRenderers.Length != 0)
+            {
+                foreach (SkinnedMeshRenderer smrenderer in skinnedMeshRenderers)
+                {
+                    foreach (Material material in smrenderer.materials)
+                    {
+                        //print("Skinned Mesh: " + clippingLevel);
+                        material.SetFloat("_ClippingLevel", clippingLevel);
+                    }
+                }
+            }
+
+            //Mesh renderer
+            MeshRenderer[] meshRenderers = this.GetComponentsInChildren<MeshRenderer>();
+
+            if (meshRenderers.Length != 0)
+            {
+                foreach (MeshRenderer mrenderer in meshRenderers)
+                {
+                    foreach (Material material in mrenderer.materials)
+                    {
+                        //print("Renderer Mesh: " + clippingLevel);
+                        material.SetFloat("_ClippingLevel", clippingLevel);
+                    }
+                }
+            }
+
+            yield return 0;
+        }
+
+        //print("Hide ended");
+        m_CurrentCoroutine = null; 
+    }
+
+    private IEnumerator ShowModel(float minClippingLevel, float maxClippingLevel) 
+    {
+        float clippingLevel = minClippingLevel;
+
+        while (clippingLevel < maxClippingLevel)
+        {
+            clippingLevel += Mathf.Abs(maxClippingLevel - minClippingLevel) / m_ClippingVelocity * Time.deltaTime;
+
+
+            ////For the parent:
+
+            SkinnedMeshRenderer parentSkinnedMeshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+
+            if (parentSkinnedMeshRenderer != null)
+            {
+                foreach (Material material in parentSkinnedMeshRenderer.materials)
+                {
+                    material.SetFloat("_ClippingLevel", clippingLevel);
+                }
+            }
+
+            MeshRenderer parentMeshRenderer = this.GetComponent<MeshRenderer>();
+
+            if (parentMeshRenderer != null)
+            {
+                foreach (Material material in parentMeshRenderer.materials)
+                {
+                    material.SetFloat("_ClippingLevel", clippingLevel);
+                }
+            }
+
+            //For the children:
+
+            SkinnedMeshRenderer[] skinnedMeshRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            if (skinnedMeshRenderers.Length != 0)
+            {
+                foreach (SkinnedMeshRenderer smrenderer in skinnedMeshRenderers)
+                {
+                    foreach (Material material in smrenderer.materials)
+                    {
+                        //print("Skinned Mesh: " + clippingLevel);
+                        material.SetFloat("_ClippingLevel", clippingLevel);
+                    }
+                }
+            }
+
+            MeshRenderer[] meshRenderers = this.GetComponentsInChildren<MeshRenderer>();
+
+            if (meshRenderers.Length != 0)
+            {
+                foreach (MeshRenderer mrenderer in meshRenderers)
+                {
+                    foreach (Material material in mrenderer.materials)
+                    {
+                        //print("Renderer Mesh: " + clippingLevel);
+                        material.SetFloat("_ClippingLevel", clippingLevel);
+                    }
+                }
+            }
+
+            yield return 0;
+        }
+
+        //print("Show ended");
+        m_CurrentCoroutine = null;
+    }
+}
---
+++ Assets/EffectsBoilerplate/Shader Graph/Displacement/DisplacementControll.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementControll : MonoBehaviour
+{
+    public float m_MaxDisplacement = 1.0f;
+    [Range(1.0f, 15.0f)]
+    public float m_MetallicLerpVelocity = 1.0f;
+
+    [Range(1.0f, 15.0f)]
+    public float m_ColorLerpVelocity = 1.0f;
+
+    private float m_DisplacementAmount = 0.0f;
+
+    private float m_ColorLerp = 0.0f;
+    private float m_MetallicAmount = 0.0f;
+
+    public ParticleSystem m_ExplosionParticles;
+    MeshRenderer m_MeshRenderer;
+
+    private bool m_DisplaceIn = false;
+
+    void Start()
+    {
+        m_MeshRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+    void Update()
+    {
+
+        if (m_DisplaceIn)
+        {
+            MetallicIn();
+            DisplaceIn();
+        }
+        else
+        {
+            DisplaceOut();
+            MetallicOut();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_DisplaceIn = !m_DisplaceIn;
+
+            if (m_ExplosionParticles)
+            {
+                m_ExplosionParticles.Play();
+            }
+            
+        }
+    }
+
+    private void DisplaceIn() 
+    {
+        m_DisplacementAmount = Mathf.Lerp(m_DisplacementAmount, m_MaxDisplacement, Time.deltaTime);
+        m_MeshRenderer.material.SetFloat("_Amount", m_DisplacementAmount);
+    }
+
+    private void DisplaceOut()
+    {
+        m_DisplacementAmount = Mathf.Lerp(m_DisplacementAmount, 0, Time.deltaTime);
+        m_MeshRenderer.material.SetFloat("_Amount", m_DisplacementAmount);
+    }
+
+    private void MetallicIn() 
+    {
+        m_MetallicAmount = Mathf.SmoothStep(m_MetallicAmount, 1, Time.deltaTime * m_MetallicLerpVelocity);
+        m_MeshRenderer.material.SetFloat("_Metallic", m_MetallicAmount);
+
+    }
+
+    private void MetallicOut()
+    {
+        m_MetallicAmount = Mathf.SmoothStep(m_MetallicAmount, 0, Time.deltaTime * m_MetallicLerpVelocity);
+        m_MeshRenderer.material.SetFloat("_Metallic", m_MetallicAmount);
+    }
+
+    private void LerpColorIn()
+    {
+        m_ColorLerp = Mathf.SmoothStep(m_ColorLerp, 1, Time.deltaTime * m_ColorLerpVelocity);
+        m_MeshRenderer.material.SetFloat("_ColorLerp", m_ColorLerp);
+    }
+
+    private void LerpColorOut()
+    {
+        m_ColorLerp = Mathf.SmoothStep(m_ColorLerp, 0, Time.deltaTime * m_ColorLerpVelocity);
+        m_MeshRenderer.material.SetFloat("_ColorLerp", m_ColorLerp);
+    }
+
+    private void Spazm() 
+    {
+        m_DisplacementAmount += m_MaxDisplacement;
+        m_ColorLerp = 1.0f;
+        m_MetallicAmount = 1.0f;
+        //Use DisplaceOut/MetallicOut in the update for the effect
+    }
+}
